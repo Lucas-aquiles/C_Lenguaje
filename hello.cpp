037c03fb20817b2b22e2d2d17a511c740bcf8973
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-06-13 22:35:50 -0300

    nuevas funciones

--- hello.cpp
+++ hello.cpp
@@ -1,31 +1,29 @@
-#include <stdio.h>
+// #include <stdio.h>
+// // int main()
+// // {
+
+// // /* Aquí va el cuerpo del programa */
+// // printf( "Hola mundo\n" ); /* Esto imprime hola mundo en pantalla */
+// // return 0;
+// // }
 // int main()
 // {
+// // variables
+// //Una declaración consiste en un tipo de datos
+// int a, b, c;
+// char d;
+// a=2;
+// b=3;
+// c=a+b;
+// d='W';
 
 
+// printf( "Hola mundo \n" ); /* Esto imprime hola mundo en pantalla */
+// printf( "Segunda" );
+// printf( "Primera cadena" "Segunda cadena" );
 
-// /* Aquí va el cuerpo del programa */
-// printf( "Hola mundo\n" ); /* Esto imprime hola mundo en pantalla */
 // return 0;
 // }
-int main()
-{
-// variables
-//Una declaración consiste en un tipo de datos
-int a, b, c;
-char d;
-a=2;
-b=3;
-c=a+b;
-d='W';
-
-
-printf( "Hola mundo \n" ); /* Esto imprime hola mundo en pantalla */
-printf( "Segunda" );
-printf( "Primera cadena" "Segunda cadena" );
-
-return 0;
-}
 
 //CONSTANTES
 
@@ -34,10 +32,57 @@ return 0;
 
 //OPERADORES
 //var1 = var2 + var3;
+// #include <iostream>
+// using namespace std;
 
-/* 
+// int main() {
+//     string cadena = "¡Hola, mundo!";
+    
+//     cout << "hola nena" << "vaca" << endl;
+    
+//     return 0;
+// }
+//introducir datos al programa
+// #include <stdio.h>
+// int main()
+// {
+// int num;
+// printf( "Introduce un número " );
+// scanf( "%i", &num );
+// printf( "Ingresamos el número %i\n", num );
+// }
 
+//introducir datos al programa
+// #include <iostream>
+// using namespace std;
+// int main()
+// { 
+//  int numero;
+//  cout << "Ingrese el 1er numero entero: ";
+//  cin >> numero;
+//  cout << "El numero ingresado es: " << numero;
+//  return 0;
+// }
 
+#include <stdio.h>
+int main()
+{
+char letra;
+printf( "Introduce una letra: " );
+fflush( stdout );
+letra = getchar();
+printf( "\nHas introducido la letra: %c", letra );
+}
+/////////////////
 
-*/
+// #include <stdio.h>
+// int main()
+// {
+// char letra;
+// printf( "Introduce una letra: " );
+// fflush( stdout );
+// letra = getchar();
+// printf("\n has introducido la letra :%c", letra );
+// }
+
 
--- primero.cpp
+++ primero.cpp
@@ -1,6 +1,47 @@
+// #include <stdio.h>
+
+// main(){
+//     printf("HOLA, ESTE ES MI PRIMER PROGRAMA EN C+d+");
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+// /* Aquí va el cuerpo del programa */
+// printf( "Hola mundo\n" ); /* Esto imprime hola mundo en pantalla */
+// return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+// cout << "Hola MUNDO, " << " Hola C++ " << endl;  
+//       return 0;
+
+// }
+
+
+// #include <stdio.h>
+// int main()
+// {
+// int num;
+// printf( "Introduce un número " );
+// scanf( "%i", &num );
+// printf( "Ingresamos el número %i\n", num );
+// }
+
+
 #include <stdio.h>
+#include <conio.h>
 
-main(){
-    printf("HOLA, ESTE ES MI PRIMER PROGRAMA EN C+d+");
+int main()
+{
+char letra;
+printf( "Introduce una letra: " );
+fflush( stdout );
+letra = getche();
+printf( "\nHas introducido la letra: %c", letra );
+return 0;
+
 }
-
